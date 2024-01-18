7c67cc96c5787f27001b439c4353ad32b4471515
Mohd Sharique Siddiqui <shariquesiddiqui100@gmail.com>
2024-01-18 16:31:32 +0000

    stack & Heap memory

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -35,3 +35,13 @@ const myFunction = function(){
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+/*Stack => All Primitive Data Type
+      => Changes Done in Copy of a Variable
+      
+
+Heap => Non-Primitive Data Type
+    => Changes Done in Original Variable
+    */
